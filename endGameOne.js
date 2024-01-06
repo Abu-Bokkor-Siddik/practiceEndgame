87367ddb9bf97a02493acd8a55d74494a2ab0456
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2024-01-06 22:12:35 +0600

    js pactices

---
+++ endGameOne.js
@@ -0,0 +1,79 @@
+// function outPut(number) {
+//     return number / 2 !== 0;
+//   }
+  
+//   function number() {
+//     for (let i = 1; i <= 50; i++) {
+//       if (outPut(i)) {
+//         console.log(i);
+//       }
+//     }
+//   }
+  
+//   number();
+
+// console.log('hello world ')
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.MIN_SAFE_INTEGER)
+// console.log(Number.MAX_VALUE)
+// let abc 
+// let bcd = null
+// console.log(abc===bcd)
+// console.log(bcd)
+
+
+// false values 
+// "",0,null,undefine,NaN
+
+ 
+
+
+// arithmetic operators
+// +,-,*,/, % ,--,++
+
+// %
+// let a = 10;
+// console.log(a%3)
+
+
+// incremental 
+// pre Incremental  and Post incremental
+// post Increment  pore 1 increment kore . pre increment age increment kore 
+// console.log(a++)
+// console.log(a)
+
+// pre increment 
+// let b = 9;
+// console.log(++b)
+
+
+// decrement -- same as increment 
+
+
+
+// 2 Assignment operators 
+//  +=,-=,*=,/=
+//  let c = 10;
+//  let d = 20;
+//  c+=d 
+//  console.log(c)
+//  c-=d
+//  console.log(c)
+//  c/=d 
+//  console.log(c)
+//  c%=d 
+//  console.log(c)
+
+
+// 3 comparison  operators
+// == ,<,>,<= ,>= ,===(type ta soho korte chaile aita ) !(bang bole mane olte deoya )
+// let e = 10 ;
+// let f = 10;
+// console.log(e<=f)
+
+
+// 4 logical  operators 
+//  && and, || or, ! not
+
+// statements 
+// js a jekono code ba line jeta kono ortho bujai take statement bole ...
